844d0efa66182047326510fa4fc388bcb754bc78
Osamu Morozumi <osamumorozumi@gmail.com>
2024-12-01 12:35:02 +0800

    standardizing source files and parser wip

---
+++ parse.php
@@ -0,0 +1,35 @@
+#!/usr/bin/env php
+<?php
+
+$markdown = file_get_contents(__DIR__ . '/thai-forest/ajahn-dune/gifts.md');
+
+$parts = array_values(array_filter(explode('---', $markdown), function ($part) {
+    // remove empty parts
+    return $part;
+}));
+
+$meta = [];
+$content = [];
+
+if (yaml_parse($parts[0])) {
+    $meta = yaml_parse($parts[0]);
+    $content = array_slice($parts,1);
+} else {
+    $content = $parts;
+}
+
+$content = array_map(function($line) {
+    $line = trim($line);
+    $line = "$line\n";
+    $line .= extractMeta();
+    return $line;
+}, $content);
+
+function extractMeta() {
+    global $meta;
+    return $meta['author'] ? "\n- " . $meta['author'] . "\n" : "";
+}
+
+$file = 'out.txt';
+
+file_put_contents($file, join("%\n", $content));
